150064c8780984633583d88113ec714b926f4d10
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2023-12-23 23:54:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let panels=document.querySelectorAll(".panel");
+function onClickToggle(e) {
+
+	for(let i=0;i<panels.length;i++){
+	
+		panels[i].classList.remove("active")
+	
+	}
+		
+		
+			e.classList.add("active");
+			
+			
+}
